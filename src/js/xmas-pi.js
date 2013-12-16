760fd50251e5c29a5309c8346974038f46f09d2b
wiped <wiped1@gmail.com>
2013-12-16 15:55:29 +0100

    delete now clears frame if frame counter equals 1

--- src/js/xmas-pi.js
+++ src/js/xmas-pi.js
@@ -206,6 +206,8 @@
                 });
             }
         } else {
+            this.clearFrame();
+            this.resetValues();
             console.log("No more frames, preventing deletion");
         }
     }
@@ -319,6 +321,9 @@
         $(this).toggleBulbs(frames);
     }
 
+    // clears class only
+    // it's not reseting values, because it's used in animation too,
+    // that's wy there are two functions that seemingly do the same
     $.fn.clearFrame = function() {
         $(this).find('button').each(function() {
             $(this).attr("value", 0);
